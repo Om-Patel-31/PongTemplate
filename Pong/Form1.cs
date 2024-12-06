fb9dac0248d30163cf02e9a86f380cbeaf7f44c9
Om Patel <om31digital@gmail.com>
2024-12-06 11:22:56 -0500

    Completed Game!

--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -21,13 +21,13 @@ namespace Pong
         int player2Score = 0;
 
         int playerSpeed = 4;
-        int ballXSpeed = -6;
-        int ballYSpeed = 6;
+        int ballXSpeed = 6;
+        int ballYSpeed = -6;
 
-        bool wDown = false;
-        bool sDown = false;
-        bool upArrowDown = false;
-        bool downArrowDown = false;
+        bool wPressed = false;
+        bool sPressed = false;
+        bool upPressed = false;
+        bool downPressed = false;
 
         SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
@@ -35,6 +35,134 @@ namespace Pong
         public Form1()
         {
             InitializeComponent();
+            p1ScoreLabel.Text = "0";
+            p2ScoreLabel.Text = "0";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
+                    break;
+                case Keys.Up: 
+                    upPressed = true;
+                    break;
+                case Keys.Down: 
+                    downPressed = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+                case Keys.Up:
+                    upPressed = false;
+                    break;
+                case Keys.Down:
+                    downPressed = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            ball.X += ballXSpeed;
+            ball.Y += ballYSpeed;
+
+
+            if (wPressed == true && player1.Y > 0)
+            {
+                player1.Y -= playerSpeed;
+            }
+
+            if (sPressed == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += playerSpeed;
+            }
+
+            if (upPressed == true && player2.Y > 0)
+            {
+                player2.Y -= playerSpeed;
+            }
+
+            if (downPressed == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += playerSpeed;
+            }
+
+            if (ball.Y < 0 || ball.Y > this.Height - ball.Height)
+            {
+                ballYSpeed *= -1;  
+            }
+
+            if (player1.IntersectsWith(ball))
+            {
+                ballXSpeed *= -1;
+                ball.X = player1.X + ball.Width;
+            }
+            else if (player2.IntersectsWith(ball))
+            {
+                ballXSpeed *= -1;
+                ball.X = player2.X - ball.Width;
+            }
+
+            if (ball.X < 0)
+            {
+                player2Score++;
+                p2ScoreLabel.Text = $"{player2Score}";
+
+                ball.X = 295;
+                ball.Y = 195;
+
+                player1.Y = 170;
+                player2.Y = 170;
+            }
+            else if (ball.X > 600)
+            {
+                player1Score++;
+                p1ScoreLabel.Text = $"{player1Score}";
+
+                ball.X = 295;
+                ball.Y = 195;
+
+                player1.Y = 170;
+                player2.Y = 170;
+            }
+
+            if (player1Score == 3)
+            {
+                gameTimer.Enabled = false;
+                winLabel.Visible = true;
+                winLabel.Text = "Player 1 Wins!!";
+            }
+            else if (player2Score == 3)
+            {
+                gameTimer.Enabled = false;
+                winLabel.Visible = true;
+                winLabel.Text = "Player 2 Wins!!";
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(blueBrush, player1);
+            e.Graphics.FillRectangle(blueBrush, player2);
+            e.Graphics.FillEllipse(whiteBrush, ball);
         }
     }
 }
